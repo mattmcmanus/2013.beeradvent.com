3ffb536711d77c4d3e7a13d45c4ace5948013a42
Matt McManus <mattmcmanus@gmail.com>
2012-11-17 22:46:34 -0500

    The calendar is starting to take shape

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 ---
 ---
-{% include javascripts/jquery.js %}
-{% include javascripts/jquery.lettering-0.6.1.min.js %}
 {% include javascripts/foundation.min.js %}
+{% include javascripts/jquery.lettering-0.6.1.min.js %}
+{% include javascripts/app.js %}
